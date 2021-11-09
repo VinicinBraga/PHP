0f52a54bc54edd8822e86495a12c45e131bef8ea
VinicinBraga <vinicius@bragamatta.com.br>
2021-11-09 13:41:36 -0300

    Praticando Formulario em PHP

---
+++ Formulario em PHP/enviar.php
@@ -0,0 +1,43 @@
+<?php
+date_default_timezone_set('America/Belo_Horizonte');
+ 
+require('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+ 
+if((isset($_POST['email']) && !empty(trim($_POST['email']))) && (isset($_POST['mensagem']) && !empty(trim($_POST['mensagem'])))) {
+ 
+	$nome = !empty($_POST['nome']) ? $_POST['nome'] : 'Não informado';
+	$email = $_POST['email'];
+	$assunto = !empty($_POST['assunto']) ? utf8_decode($_POST['assunto']) : 'Não informado';
+	$mensagem = $_POST['mensagem'];
+	$data = date('d/m/Y H:i:s');
+ 
+	$mail = new PHPMailer();
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'vinicius@bragamatta.com.br';
+	$mail->Password = '';
+	$mail->Port = 587;
+ 
+	$mail->setFrom('vinicius@bragamatta.com.br');
+	$mail->addAddress('vinicius@bragamatta.com.br');
+ 
+	$mail->isHTML(true);
+	$mail->Subject = $assunto;
+	$mail->Body = "Nome: {$nome}<br>
+				   Email: {$email}<br>
+				   Mensagem: {$mensagem}<br>
+				   Data/hora: {$data}";
+ 
+	if($mail->send()) {
+		echo 'Email enviado com sucesso.';
+	} else {
+		echo 'Email não enviado.';
+	}
+} else {
+	echo 'Não enviado: informar o email e a mensagem.';
+}
